55f86bbc5c1877624f0b00dec4ef39fb0d0519a6
anto.redrobot <nurhidayanto@redrobot.co.id>
2025-07-07 18:49:41 +0700

    userDirectory

--- userDirectory/src/components/UserCard.jsx
+++ userDirectory/src/components/UserCard.jsx
@@ -1,27 +1,48 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 
 const UserCard = () => {
-    const [user, setUsers] = useState('')
+    const [users, setUsers] = useState([])
 
-const getUser = () => {
-    const response = axios.get('https://jsonplaceholder.typicode.com/users').then(resp => {
-        setUsers(resp.data)
-        console.log(response)
+const getUser = async () => {
+    await axios.get('https://jsonplaceholder.typicode.com/users').then(response => {
+        console.log(response.data)
+        setUsers(response.data)
     }).catch(err => {
         console.error("error:",err)
     })
 }
 
 useEffect(()=> {
-    getUser()
+    
 },[])
+
+const handleView = () => {
+    getUser()
+}
 
 
   return (
     <>
-    <div>UserCard</div>
-    <h1>Username</h1>
-    <h2>Email</h2>
+    <div className="container">
+        <div className="left-column">
+            <h1>Halo</h1>
+            <h3>Untuk tahu user klik tombol di bawah ini</h3>
+            <button onClick={handleView}>View User</button>
+        </div>
+         <div className="right-column">
+                {
+                    users.map((user, id) => {
+                        return (
+                            <div key={id} className="card">
+                                <h1>{user.username}</h1>
+                                <h2>{user.email}</h2>
+                            </div>
+                        )
+                    })
+                }
+        </div>
+    </div>
     </>
   )
 }
